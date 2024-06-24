079eb7968f110b7618ef4f935550b49005f46ad2
Chetan Rudrakar <chetan.rudrakar@zebra.com>
2024-06-24 23:10:17 +0530

    Actions

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -41,8 +41,6 @@ test.beforeEach( async ({page}) =>{
         const pm=new PageManager(page)
         await  pm.getNavigationPage().formLayoutPage();
         await  pm.getNavigationPage().datePickerPage();
-        
-        
 
     })
 
